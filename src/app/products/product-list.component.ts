28778a4d1ecf8bf3605e68295ad23fab1ba53ffe
Sophie Jiang <sifeij@gmail.com>
2023-09-23 15:39:30 -0700

    build nested component with @Input() @Output()

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit {
     }
     set listFilter(value: string) {
         this._listFilter = value;
-        console.log('In setter:', value);
+        // console.log('In setter:', value);
         this.filteredProducts = this.performFilter(value);
     }
 
@@ -26,31 +26,62 @@ export class ProductListComponent implements OnInit {
 
     products: IProduct[] = [
         {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          }
-    ];
+          "productId": 1,
+          "productName": "Leaf Rake",
+          "productCode": "GDN-0011",
+          "releaseDate": "March 19, 2021",
+          "description": "Leaf rake with 48-inch wooden handle.",
+          "price": 19.95,
+          "starRating": 3.2,
+          "imageUrl": "assets/images/leaf_rake.png"
+        },
+        {
+          "productId": 2,
+          "productName": "Garden Cart",
+          "productCode": "GDN-0023",
+          "releaseDate": "March 18, 2021",
+          "description": "15 gallon capacity rolling garden cart",
+          "price": 32.99,
+          "starRating": 4.2,
+          "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+          "productId": 5,
+          "productName": "Hammer",
+          "productCode": "TBX-0048",
+          "releaseDate": "May 21, 2021",
+          "description": "Curved claw steel hammer",
+          "price": 8.9,
+          "starRating": 4.8,
+          "imageUrl": "assets/images/hammer.png"
+        },
+        {
+          "productId": 8,
+          "productName": "Saw",
+          "productCode": "TBX-0022",
+          "releaseDate": "May 15, 2021",
+          "description": "15-inch steel blade hand saw",
+          "price": 11.55,
+          "starRating": 3.7,
+          "imageUrl": "assets/images/saw.png"
+        },
+        {
+          "productId": 10,
+          "productName": "Video Game Controller",
+          "productCode": "GMG-0042",
+          "releaseDate": "October 15, 2020",
+          "description": "Standard two-button video game controller",
+          "price": 35.95,
+          "starRating": 4.6,
+          "imageUrl": "assets/images/xbox-controller.png"
+        }
+      ];
 
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
-        return this.products.filter((product: IProduct) =>
+        let result: IProduct[] = this.products.filter((product: IProduct) =>
             product.productName.toLocaleLowerCase().includes(filterBy));
+        return result;
     }
 
     toggleImage(): void {
@@ -58,6 +89,14 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this._listFilter = 'cart';
+        this.listFilter = 'cart';
+
+        // had to add this to make initial filter on page load work
+        // or change this._listFilter = 'cart'; to be without underscore
+        // this.filteredProducts = this.performFilter(this._listFilter);
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
 }
